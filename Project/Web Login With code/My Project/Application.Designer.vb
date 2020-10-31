098e2a930a21d0a22e2201883942b4569bc92a24
X7md <54203033+X7md@users.noreply.github.com>
2020-10-31 20:22:53 +0300

    Delete membership form

--- Project/Web Login With code/My Project/Application.Designer.vb
+++ Project/Web Login With code/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.Web_Login_With_code.LOGIN
+            Me.MainForm = Global.Web_Login_With_code.Form1
         End Sub
     End Class
 End Namespace
